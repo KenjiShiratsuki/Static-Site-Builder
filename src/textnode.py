d10b7ff6cac950b9009dab82ab94226c0aee2ef9
KenjiShiratsuki <kenjishiratsuki@gmail.com>
2025-04-11 12:59:14 -0400

    Chapter 2, Lesson 1 complete

---
+++ src/textnode.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+class TextType(Enum):
+    NORMAL_TEXT = "normal"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class TextNode:
+    def __init__(self, TEXT, TEXT_TYPE, URL=None):
+        self.text = TEXT
+        self.text_type = TEXT_TYPE
+        self.url = URL
+    
+    def __eq__(self, other):
+        if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
+            return True
+        else:
+            return False
+        
+    def __repr__(self):
+        return(f"TextNode({self.text}, {self.text_type.value if hasattr(self.text_type, 'value') else self.text_type}, {self.url})")
