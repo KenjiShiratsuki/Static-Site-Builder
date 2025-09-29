b64879a4371e019b1eec4d8fa0c435c65e8236ea
KenjiShiratsuki <kenjishiratsuki@gmail.com>
2025-09-29 16:57:45 -0400

    Ch5 L4 Complete

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from block_to_html import markdown_to_html_node  # adjust import to your module name
 from extract_title import extract_title          # adjust import to your module name
 
@@ -29,3 +30,21 @@ def write_file(dest_path, content):
         os.makedirs(dirpath, exist_ok=True)
     with open(dest_path, "w", encoding="utf-8") as f:
         f.write(content)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    print(f"Scanning '{dir_path_content}' for markdown files to convert:")
+
+    mdfiles = [os.path.join(root, name)
+             for root, dirs, files in os.walk(dir_path_content)
+             for name in files
+             if name.endswith(".md")]
+
+    print(mdfiles)
+
+    for file in mdfiles:
+        print(f"Processing '{file}'")
+        source_path = file
+        dest_path = Path(dest_dir_path, *Path(file).parts[1:]).with_suffix('.html')
+
+        print(f"Converting and writing to '{dest_path}'")
+        generate_page(source_path, template_path, dest_path)
